69c2dbd84468c672be77ca1911f4473e99bbac66
rupa <rupa@lrrr.us>
2016-07-29 00:57:41 -0400

    add demo

---
+++ demo.py
@@ -0,0 +1,24 @@
+from euclid import BeatStepPro
+from euclid import EuclideanSequence
+from euclid import OctoEuclid
+
+
+slots = [
+    EuclideanSequence(9, 17),
+    EuclideanSequence(3, 7),
+    EuclideanSequence(4, 8),
+    EuclideanSequence(3, 5),
+    EuclideanSequence(7, 11),
+    EuclideanSequence(2, 3),
+    EuclideanSequence(6, 8),
+    EuclideanSequence(11, 19),
+]
+
+oe = OctoEuclid(slots)
+print oe
+
+bsp = BeatStepPro(oe)
+print bsp
+
+from IPython import embed
+embed()
